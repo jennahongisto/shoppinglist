34336fdbd120ddebc8f37a10515cceb6a0673ba1
JennaHongisto <n2hoje00@students.oamk.fi>
2023-03-14 14:09:31 +0200

    created php

---
+++ db.sql
@@ -0,0 +1,13 @@
+create database shoppinglist;
+
+use shoppinglist;
+
+create table item (
+    id int primary key auto_increment,
+    description varchar(255) not null,
+    amount smallint unsigned not null
+);
+
+insert into item (description, amount) values ('Banaani', 2); 
+insert into item (description, amount) values ('Omena', 5); 
+insert into item (description, amount) values ('Kiivi', 10); 
